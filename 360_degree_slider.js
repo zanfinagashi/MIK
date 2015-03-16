6a6a84772a180ffc427c82e9411c3093c7fe722e
zanfinagashi <zanfinagashi@gmail.com>
2015-03-16 10:00:27 +0100

    Slider

--- 360_degree_slider.js
+++ 360_degree_slider.js
@@ -1,37 +1,32 @@
 
 
 $( document ).ready(function() {
-      
-
-
-//var interval = self.setInterval(function(){
 
 	
-
-	var links = ['google','google','facebook','facebook','twitter','twitter','linkedin','linkedin','linkedin'];
+	var pos = [0,-282.593,-565.186,-847.779,-1130.372,-1412.965,-1695.558,-1978.151,-2260.744];
+	var links = ['google','google','facebook','facebook','twitter','twitter','linkedin','linkedin','google'];
 
   
-    var time = 1000;
+    var time = 500;
 
-	$.each(links,function(index)
+	$.each(pos,function(index)
 	{
 	
 		setTimeout(function() {			
          
-    	$('.product-carousel').css('-webkit-transform','translateY(' + index*(-288) + 'px)'); 
+    	$('.product-carousel').css('-webkit-transform','translateY(' + pos[index] + 'px)'); 
     	$('.link').attr("href", "http://www."+ links[index]+".com")
 		}, time);
          
          //if(index%2==0)
          	//time+=1500;
          //else
-		time += 1000;
+		time += 500;
 		
 	});
 
 	
-
-//},1000);
-
-
+      
 });
+
+
