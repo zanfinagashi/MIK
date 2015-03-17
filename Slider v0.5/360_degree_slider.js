53b8b6762156510f59267743a88e098c96387049
zanfinagashi <zanfinagashi@gmail.com>
2015-03-17 15:18:31 +0100

    Slider

--- Slider v0.5/360_degree_slider.js
+++ Slider v0.5/360_degree_slider.js
@@ -3,15 +3,7 @@
   element: document.getElementById('bg-div-id'),
   handler: function() {
 
-    $("#bg-div-id").css({
-
-      position: "fixed",
-    	top: "0px",   
-
-    });
-
-     
-
+   
 
     var lastScrollTop = 0, delta = 5, i=0;
     $(window).scroll(function(event){
@@ -24,16 +16,24 @@
        if (st > lastScrollTop){
            // downscroll code
            console.log('scroll down');
-           if(i>=0 && i<16)
-            i = i + 1;
-          
+             if(i==16)
+               {
+                  i=0;
+               }
+
+           if(i>=0 && i<=12)
+           {  
+
+              i = i + 1;
+            console.log(i );
             var time=70; 
             var pos=[4,5,6];
-           $.each(pos,function(index){
+           $.each(pos,function(){
               
                 setTimeout(function(){
                         
                         i++;
+                        console.log(i + "in loop plus " +  Math.floor(Date.now() / 1000) );
                         $('.product-carousel').css('-webkit-transform','translateY(' + i*(-236) + 'px)');
                         
                 }, time);
@@ -44,24 +44,52 @@
 
            //this line will be executed before the each loop
             $('.product-carousel').css('-webkit-transform','translateY(' + (i)*(-236) + 'px)');
+
+           }
+            
                   
        } else {
           // upscroll code
           console.log('scroll up');
-          if(i>0 && i<=16)
-            i = i - 1;
-          console.log(i);
+         if(i==0)
+               {
+                  i=16;
+               }
+
+          if(i>=4 && i<=16)
+          {
+             i = i - 1;
+            console.log(i);
           
+          var time=70; 
+          var pos=[4,5,6];
+           $.each(pos,function(){
+              
+                setTimeout(function(){
+                        
+                        i--;
+                        console.log(i + "in loop minus");
+                        $('.product-carousel').css('-webkit-transform','translateY(' + i*(-236) + 'px)');
+                        
+                }, time);
+                        
+                        time+=70;
+             
+                    });
+
+           //this line will be executed before the each loop
+            $('.product-carousel').css('-webkit-transform','translateY(' + (i)*(-236) + 'px)');
+          
+          }
+           
        }
 
        
                               
        lastScrollTop = st; 
-       if(i==16)
-       {
-       }
-
+      
     });
 }
 })
 
+  Date.now = function() { return new Date().getTime(); }
